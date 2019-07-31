ba27c7a271fcf8d576b5c7ae95aa5d48d3f1e80f
Ricardo Gaya <rrgaya@gmail.com>
2019-07-31 14:18:46 -0300

    [Closes #3] adicionando novos modelos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import OrderItem, Items, Order
 
-# Register your models here.
+
+admin.site.register(Items)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Items(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Item"
+        verbose_name_plural = "Item"
+
+
+class OrderItem(models.Model):
+    item = models.ForeignKey(Items, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.item
+
+    class Meta:
+        verbose_name = "Ordem Item"
+        verbose_name_plural = "Ordem Item"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = "Order"
+        verbose_name_plural = "Order"
